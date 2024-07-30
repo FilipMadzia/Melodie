6f78404981ca276e774dd926711869c870e0b558
Filip Madzia <filipmadzia33@gmail.com>
2024-07-30 15:02:50 +0200

    Add BaseRepository

--- Melodie.API/Repositories/BaseRepository.cs
+++ Melodie.API/Repositories/BaseRepository.cs
@@ -1,5 +1,15 @@
-﻿namespace Melodie.API.Repositories;
+﻿using Melodie.API.Data.Entities;
+using Melodie.API.Data.Enums;
+using Microsoft.EntityFrameworkCore;
 
-public class BaseRepository
+namespace Melodie.API.Repositories;
+
+public abstract class BaseRepository<T>(MelodieAPIContext _context) : IBaseRepository<T> where T : BaseEntity
 {
+	public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>()
+		.Where(x => x.EntityStatus == EntityStatus.Active)
+		.ToListAsync();
+
+	public async Task<T?> GetById(Guid id) => await _context.Set<T>()
+		.FirstOrDefaultAsync(x => x.Id == id && x.EntityStatus == EntityStatus.Active);
 }

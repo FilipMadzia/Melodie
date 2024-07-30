700a8c43a0bcd683f46c2263ceb7b377b6bc08f0
Filip Madzia <filipmadzia33@gmail.com>
2024-07-30 15:06:28 +0200

    Made get methods virtual

--- Melodie.API/Repositories/BaseRepository.cs
+++ Melodie.API/Repositories/BaseRepository.cs
@@ -6,10 +6,10 @@ namespace Melodie.API.Repositories;
 
 public abstract class BaseRepository<T>(MelodieAPIContext _context) : IBaseRepository<T> where T : BaseEntity
 {
-	public async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>()
+	public virtual async Task<IEnumerable<T>> GetAllAsync() => await _context.Set<T>()
 		.Where(x => x.EntityStatus == EntityStatus.Active)
 		.ToListAsync();
 
-	public async Task<T?> GetById(Guid id) => await _context.Set<T>()
+	public virtual async Task<T?> GetById(Guid id) => await _context.Set<T>()
 		.FirstOrDefaultAsync(x => x.Id == id && x.EntityStatus == EntityStatus.Active);
 }

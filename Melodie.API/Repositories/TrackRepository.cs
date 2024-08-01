ed056344c17f703660524b601ea718921e54444c
Bułas <103049804+PatrykMarkiel@users.noreply.github.com>
2024-08-01 19:53:26 +0200

    Added TrackRepository (#35)

---
+++ Melodie.API/Repositories/TrackRepository.cs
@@ -0,0 +1,18 @@
+﻿using Melodie.API.Data.Entities;
+using Melodie.API.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Melodie.API.Repositories;
+
+public class TrackRepository(MelodieAPIContext context) : BaseRepository<TrackEntity>(context)
+{
+    readonly MelodieAPIContext _context = context;
+
+    public override async Task<IEnumerable<TrackEntity>> GetAllAsync() => await _context.Tracks
+        .Include(x => x.MusicGenres)
+        .Where(x => x.EntityStatus == EntityStatus.Active)
+        .ToListAsync();
+
+    public override async Task<TrackEntity?> GetById(Guid id) => await _context.Tracks
+        .FirstOrDefaultAsync(x => x.Id == id && x.EntityStatus == EntityStatus.Active);
+}

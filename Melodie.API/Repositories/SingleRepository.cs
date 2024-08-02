481b11290e9cb6d789bcca2d6b7bd7096a5712d7
Bułas <patrykmarkiel2137@gmail.com>
2024-08-02 18:21:18 +0200

    Added SingleRepository

---
+++ Melodie.API/Repositories/SingleRepository.cs
@@ -0,0 +1,21 @@
+﻿using Melodie.API.Data.Entities;
+using Melodie.API.Data.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Melodie.API.Repositories;
+
+public class SingleRepository(MelodieAPIContext context) : BaseRepository<SingleEntity>(context)
+{
+    readonly MelodieAPIContext _context = context;
+
+    public override async Task<IEnumerable<SingleEntity>> GetAllAsync() => await _context.Singles
+        .Include(x => x.TrackEntity)
+            .ThenInclude(x => x.MusicGenres)
+        .Where(x => x.EntityStatus == EntityStatus.Active)
+        .ToListAsync();
+
+    public override async Task<SingleEntity?> GetById(Guid id) => await _context.Singles
+        .Include(x => x.TrackEntity)
+            .ThenInclude(x => x.MusicGenres)
+        .FirstOrDefaultAsync(x => x.Id == id && x.EntityStatus == EntityStatus.Active);
+}

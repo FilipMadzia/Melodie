6fc673bea5ca33834dee208cc243adac5650b328
Filip Madzia <filipmadzia33@gmail.com>
2024-08-12 21:17:23 +0200

    Update base repository (#50)

--- Melodie.API/Repositories/Shared/IBaseRepository.cs
+++ Melodie.API/Repositories/Shared/IBaseRepository.cs
@@ -5,4 +5,5 @@ public interface IBaseRepository<T> where T : class
     /// <returns>All entities where EntityStatus is Active</returns>
     Task<IEnumerable<T>> GetAllAsync();
     Task<T?> GetByIdAsync(Guid id);
+    Task AddAsync(T entity);
 }

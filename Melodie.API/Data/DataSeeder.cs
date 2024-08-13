6f7fd9e965a1b7b8477b05f4ea0d1b131273385f
Filip Madzia <filipmadzia33@gmail.com>
2024-08-13 21:15:53 +0200

    Music genres seeder

--- Melodie.API/Data/DataSeeder.cs
+++ Melodie.API/Data/DataSeeder.cs
@@ -41,10 +41,23 @@ public class DataSeeder
 	{
 		MusicGenres =
 		[
+			// Three Days Grace
 			new() { Name = "Post-grunge" },
 			new() { Name = "Hard rock" },
 			new() { Name = "Alternative rock" },
-			new() { Name = "Alternative metal" }
+			new() { Name = "Alternative metal" },
+			new() { Name = "Nu metal" },
+			// syudou
+			new() { Name = "J-Pop"},
+			// Gibbs
+			new() { Name = "Hip-hop" },
+			// Ashton Irwni
+			new() { Name = "Pop rock" },
+			new() { Name = "Pop" },
+			new() { Name = "Pop punk" },
+			new() { Name = "Power pop" },
+			new() { Name = "Rock" },
+			new() { Name = "New wave" }
 		];
 	}
 
